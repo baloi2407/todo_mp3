85b079e4046b8a895d9d6fb9ffcdbc764de3367b
Ba Loi <nguyenbaloi2407@gmail.com>
2024-11-16 12:33:23 +0700

    v3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   const [work, setWork] = useState('')
-  console.log(work)
-
+  const [todos, setTodos] = useState([])
+  const handleAdd = () => {
+    if (todos?.some(item => item.id === work?.replace(/\s/g, ''))) {
+      toast.warn('Công việc đã được thêm vào trước đó')
+    } else {
+      setTodos(prev => [...prev, { id: work?.replace(/\s/g, ''), job: work }])
+      setWork('')
+    }
+  }
+  const handleDeleteJob = (id) => {
+    setTodos(prev => prev.filter(item => item.id !== id))
+  }
   return (
-    <div className="flex gap-8 justify-center items-center h-screen">
-      <input 
-      type="text" 
-      className="outline-none border border-blue-600 px-4 py-2 w-[400px]" value={work} onChange={e => setWork(e.target.value)} />
-      <button type="button" className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white">Add</button>
-    </div>
+    <>
+      <div className="flex flex-col gap-8 justify-center items-center h-screen">
+        <div className="flex gap-8">
+          <input
+            type="text"
+            className="outline-none border border-blue-600 px-4 py-2 w-[400px]" value={work} onChange={e => setWork(e.target.value)} />
+          <button type="button" className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white" onClick={handleAdd}>Add</button>
+        </div>
+        <div>
+          <h3 className="font-bold text-xl">Content:</h3>
+          <ul>
+            {todos?.map((item) => {
+              return (
+                <li key={item.id} className="flex gap-10 items-center">
+                  <span className="my-2">{item.job}</span>
+                  <span onClick={() => handleDeleteJob(item.id)} className="my-2 cursor-pointer p-2">X</span>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
   );
 }
 
